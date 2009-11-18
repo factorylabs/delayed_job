f21ad7ee3f5616ff7dce4ebce7fdc2863ea51283
Jeremy Jackson <jejacks0n@gmail.com>
2009-11-17 17:03:47 -0700

    Added support for ActiveResource (works like ActiveRecord by only serializing the id, and calling find again, so consider using caching in conjunction with this fix)

--- lib/delayed/performable_method.rb
+++ lib/delayed/performable_method.rb
@@ -1,7 +1,8 @@
 module Delayed
   class PerformableMethod < Struct.new(:object, :method, :args)
-    CLASS_STRING_FORMAT = /^CLASS\:([A-Z][\w\:]+)$/
-    AR_STRING_FORMAT    = /^AR\:([A-Z][\w\:]+)\:(\d+)$/
+    CLASS_STRING_FORMAT           = /^CLASS\:([A-Z][\w\:]+)$/
+    ACTIVERECORD_STRING_FORMAT    = /^ACTIVERECORD\:([A-Z][\w\:]+)\:(\d+)$/
+    ACTIVERESOURCE_STRING_FORMAT  = /^ACTIVERESOURCE\:([A-Z][\w\:]+)\:(\d+)$/
 
     def initialize(object, method, args)
       raise NoMethodError, "undefined method `#{method}' for #{self.inspect}" unless object.respond_to?(method)
@@ -13,14 +14,16 @@ module Delayed
     
     def display_name  
       case self.object
-      when CLASS_STRING_FORMAT then "#{$1}.#{method}"
-      when AR_STRING_FORMAT    then "#{$1}##{method}"
+      when CLASS_STRING_FORMAT          then "#{$1}.#{method}"
+      when ACTIVERECORD_STRING_FORMAT   then "#{$1}##{method}"
+      when ACTIVERESOURCE_STRING_FORMAT then "#{$1}##{method}"
       else "Unknown##{method}"
       end      
     end    
 
     def perform
       load(object).send(method, *args.map{|a| load(a)})
+    #ActiveResource::ResourceNotFound
     rescue ActiveRecord::RecordNotFound
       # We cannot do anything about objects which were deleted in the meantime
       true
@@ -30,22 +33,28 @@ module Delayed
 
     def load(arg)
       case arg
-      when CLASS_STRING_FORMAT then $1.constantize
-      when AR_STRING_FORMAT    then $1.constantize.find($2)
+      when CLASS_STRING_FORMAT          then $1.constantize
+      when ACTIVERECORD_STRING_FORMAT   then $1.constantize.find($2)
+      when ACTIVERESOURCE_STRING_FORMAT then $1.constantize.find($2)
       else arg
       end
     end
 
     def dump(arg)
       case arg
-      when Class              then class_to_string(arg)
-      when ActiveRecord::Base then ar_to_string(arg)
+      when Class                then class_to_string(arg)
+      when ActiveRecord::Base   then active_record_to_string(arg)
+      when ActiveResource::Base then active_resource_to_string(arg)
       else arg
       end
     end
 
-    def ar_to_string(obj)
-      "AR:#{obj.class}:#{obj.id}"
+    def active_resource_to_string(obj)
+      "ACTIVERESOURCE:#{obj.class}:#{obj.id}"
+    end
+
+    def active_record_to_string(obj)
+      "ACTIVERECORD:#{obj.class}:#{obj.id}"
     end
 
     def class_to_string(obj)
